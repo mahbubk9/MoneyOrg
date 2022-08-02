0bfa4d312a04330034848ca8571df95e0a80e0f3
Mahbub khan <mahbubk@live.com>
2022-08-02 18:29:58 +0100

    Commit with failing build

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,13 +9,15 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+
     .AddBlazorise(options =>
     {
         options.Immediate = true;
     })
     .AddBootstrapProviders()
     .AddFontAwesomeIcons();
+    
+
 
 await builder.Build().RunAsync();
